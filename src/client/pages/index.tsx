c5af7e4a1db1ef80cdea99a4f752c0cf84a421e5
dayasudhan <dayasudhankg@gmail.com>
2022-08-02 20:20:13 +0530

    ok

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <h1>Hello, netra Farmify!</h1>;
+  return <h1>Hello, Farmify!</h1>;
 };
 
 export default Home;
