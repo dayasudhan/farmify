f8a2753656cde9cf1d57035545f52b5d03272c81
dayasudhan <dayasudhankg@gmail.com>
2022-08-07 20:13:07 +0530

    git ignore

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,8 +1,15 @@
 import { React } from 'react';
 import { NextPage } from 'next';
-
+import Link from 'next/link';
 const Home: NextPage = () => {
-  return <h1>Hello, Farmify!</h1>;
+  return (
+    <h1 className="title">
+      Read{' '}
+      <Link href="/ui/carousal">
+        <a>Carousal!</a>
+      </Link>
+    </h1>
+  );
 };
 
 export default Home;
---
+++ src/client/pages/ui/carousal.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Carousel from 'semantic-ui-carousel-react';
+import { Image, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import Link from 'next/link';
+const App = () => {
+  let elements = [
+    {
+      render: () => {
+        return (
+          <Image src="https://i.pinimg.com/originals/09/6d/f0/096df0eb195b8f0d9475924f9a1e9425.jpg" />
+        );
+      },
+    },
+    {
+      render: () => {
+        return <Image src="https://i.imgur.com/0eRe75Y.jpg" />;
+      },
+    },
+    {
+      render: () => {
+        return (
+          <Image src="https://flipwallpapers.com/wallpapers/anime-wallpaper-hd-resolution-For-desktop-Wallpaper.jpg" />
+        );
+      },
+    },
+  ];
+  return (
+    <div style={{ width: 500 }}>
+      <h1>Carousal Example</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <Carousel
+        elements={elements}
+        duration={3000}
+        animation="slide left"
+        showNextPrev={false}
+        showIndicators={true}
+      />
+    </div>
+  );
+};
+export default App;
